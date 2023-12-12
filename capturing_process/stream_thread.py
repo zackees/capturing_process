3a78c9f4b8767ff3b670123accf4ad858e48cd9d
zackees <z@zackees.com>
2023-12-11 21:27:58 -0800

    fix linting

--- capturing_process/stream_thread.py
+++ capturing_process/stream_thread.py
@@ -15,7 +15,7 @@ def _async_raise(tid, excobj):
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(excobj))
     if res == 0:
         raise ValueError("nonexistent thread id")
-    elif res > 1:
+    if res > 1:
         # """if it returns a number greater than one, you're in trouble,
         # and you should call it again with exc=NULL to revert the effect"""
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
@@ -63,10 +63,10 @@ class StreamThread(threading.Thread):
             try:
                 self.join(timeout=0.1)
                 return
-            except Exception:  # noqa
+            except Exception:  # pylint: disable=broad-except
                 try:
                     self.terminate()
-                except Exception:  # noqa
+                except Exception:  # pylint: disable=broad-except
                     pass
 
     def to_string(self) -> str:
@@ -76,13 +76,15 @@ class StreamThread(threading.Thread):
         return out
 
     def raise_exc(self, excobj):
+        """Raises an exception in the threads with id, tid"""
         assert self.is_alive(), "thread must be started"
-        for tid, tobj in threading._active.items():
+        for tid, tobj in threading._active.items():  # pylint: disable=W0212
             if tobj is self:
                 _async_raise(tid, excobj)
                 return
 
     def terminate(self):
+        """Terminates the thread."""
         # must raise the SystemExit type, instead of a SystemExit() instance
         # due to a bug in PyThreadState_SetAsyncExc
         self.raise_exc(SystemExit)
