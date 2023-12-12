463e27b5ee61cdbba649f9b7c79f967cfe137db6
zackees <z@zackees.com>
2023-12-11 21:25:37 -0800

    allows fast thread exit when it refused to join fast

--- capturing_process/stream_thread.py
+++ capturing_process/stream_thread.py
@@ -5,11 +5,22 @@ Stream for capturing subprocess streams to a buffer.
 import threading
 from io import StringIO
 from typing import Any
+import ctypes
 
 # ignore E203
 # flake8: noqa:E203
 
-THREAD_JOIN_TIMEOUT = 5
+
+def _async_raise(tid, excobj):
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(excobj))
+    if res == 0:
+        raise ValueError("nonexistent thread id")
+    elif res > 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+
 
 class StreamThread(threading.Thread):
     """Internal class for streaming subprocess streams to a buffer."""
@@ -46,14 +57,32 @@ class StreamThread(threading.Thread):
         if self.out_stream:
             self.out_stream.write(out)
 
-    def join_once(self) -> Any:
+    def join_once(self) -> None:
         """Like join() but safe for multiple calls."""
-        if not self.dead:
-            self.dead = True
-            self.join(timeout=THREAD_JOIN_TIMEOUT)
+        if not self.dead and self.is_alive():
+            try:
+                self.join(timeout=0.1)
+                return
+            except Exception:  # noqa
+                try:
+                    self.terminate()
+                except Exception:  # noqa
+                    pass
 
     def to_string(self) -> str:
         """Converts the whole buffer into a string."""
         with self.buffer_lock:
             out = self.buffer.getvalue()
         return out
+
+    def raise_exc(self, excobj):
+        assert self.is_alive(), "thread must be started"
+        for tid, tobj in threading._active.items():
+            if tobj is self:
+                _async_raise(tid, excobj)
+                return
+
+    def terminate(self):
+        # must raise the SystemExit type, instead of a SystemExit() instance
+        # due to a bug in PyThreadState_SetAsyncExc
+        self.raise_exc(SystemExit)
